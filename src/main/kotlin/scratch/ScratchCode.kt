19875d19013ec4cdd9e5087de385c65676efcbf5
Aras <amvitkus@gmail.com>
2022-09-19 14:28:30 -0400

    Created ScratchCode.kt to run some test functions. First function for looping through an array to find a specific item in the array.

---
+++ src/main/kotlin/scratch/ScratchCode.kt
@@ -0,0 +1,19 @@
+package scratch
+
+//Kotlin file to run some test scratch code
+
+//Array of strings
+val frodo = arrayOf<String>("Aragorn", "Boromir", "Frodo", "Sam", "Legolas")
+
+//Main Kotlin function
+fun main(){
+    println("Scratch testing code.")
+    findFrodo(frodo)
+}
+
+//Function to go through indices of array to find specific string item
+fun findFrodo (myArray: Array<String>) {
+    for (item in myArray.indices)
+        if (myArray[item] == "Frodo")
+            println("Found Frodo, Sauron wins.")
+}

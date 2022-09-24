406228ca76b169bd1ef46f5b5a669470b45c01a6
Aras <amvitkus@gmail.com>
2022-09-23 21:25:18 -0400

    Finished reversing a string.

--- src/main/kotlin/datastructures/arrays/reversestring/ReverseString.kt
+++ src/main/kotlin/datastructures/arrays/reversestring/ReverseString.kt
@@ -1,2 +1,23 @@
 package datastructures.arrays.reversestring
 
+//Creating a function to reverse a string
+//"Welcome to Middle-Earth"
+//"htraE-elddiM ot emocleW
+
+fun main() {
+    var myStr = "Welcome to Middle-Earth"
+    val myStrTwo = "For Frodo!"
+    myStr = reverseStr(myStr)
+    println(myStr)
+    //Built-in reverse function for strings
+    println(myStrTwo.reversed())
+
+}
+
+fun reverseStr(str: String) : String {
+    var result = ""
+    for (i in str.length - 1 downTo 0) {
+        result += str[i]
+    }
+    return result
+}

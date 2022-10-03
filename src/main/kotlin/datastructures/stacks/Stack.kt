dbe2a0ca2f0925a636dd13ad51f50b99f4d3974e
Aras <amvitkus@gmail.com>
2022-10-03 14:34:32 -0400

    Created Stack.kt to implement a Stack with its methods.

---
+++ src/main/kotlin/datastructures/stacks/Stack.kt
@@ -0,0 +1,64 @@
+package datastructures.stacks
+
+import java.util.*
+
+fun main() {
+
+    val myStack = Stack()
+    myStack.push("Subaru")
+    myStack.push("Honda")
+    myStack.push("Toyota")
+    myStack.push("Mazda")
+    println(myStack.peek())
+    myStack.pop()
+    println(myStack.isEmpty())
+    println(myStack.lastElement)
+}
+
+//Implementation of what is in a Stack
+class Stack {
+    private var top: Node? = null
+    private var bottom: Node? = null
+    private var length = 0
+
+    fun peek(): String? {
+        return if (length > 0) {
+            top!!.value
+        } else {
+            null
+        }
+    }
+
+    fun push(value: String?) {
+        val newNode = Node(value!!)
+        if (length == 0) {
+            top = newNode
+            bottom = newNode
+        } else {
+            newNode.next = top
+            top = newNode
+        }
+        length++
+    }
+
+    fun pop() {
+        if (length > 0) {
+            top = top!!.next
+            if (length == 1) {
+                bottom = null
+            }
+            length--
+        }
+    }
+
+    fun isEmpty() = length == 0
+
+    val lastElement: String?
+        get() = if (length > 0) {
+            bottom!!.value
+        } else null
+}
+
+class Node(var value: String) {
+    var next: Node? = null
+}

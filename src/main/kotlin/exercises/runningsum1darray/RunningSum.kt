da9a95b72dfd25367fc363f771d2a4cb67ca3c94
Aras <amvitkus@gmail.com>
2022-11-01 13:05:43 -0400

    Finished RunningSum exercise.

---
+++ src/main/kotlin/exercises/runningsum1darray/RunningSum.kt
@@ -0,0 +1,34 @@
+package exercises.runningsum1darray
+
+/*
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+*/
+
+fun main(){
+    val solution = Solution()
+    val myNums = intArrayOf(1, 2, 3, 4)
+    println(solution.runningSum(myNums))
+}
+
+//Working function for solution on LeetCode, problem 1480
+class Solution {
+    fun runningSum(nums: IntArray): IntArray {
+        if (nums.size == 1) {
+            return nums
+        }
+        for (index in 1 until nums.size) {
+            nums[index] += nums[index - 1]
+        }
+        return nums
+    }
+}
+

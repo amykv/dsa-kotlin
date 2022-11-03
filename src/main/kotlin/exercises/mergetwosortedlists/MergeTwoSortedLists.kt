2c43a7f8cad4c948501bbcf0e5ef340db4ff9fd4
Aras <amvitkus@gmail.com>
2022-11-03 12:33:37 -0400

    Started MergeTwoSortedLists.kt.

--- src/main/kotlin/exercises/mergetwosortedlists/MergeTwoSortedLists.kt
+++ src/main/kotlin/exercises/mergetwosortedlists/MergeTwoSortedLists.kt
@@ -34,8 +34,32 @@ fun main() {
  *     var next: ListNode? = null
  * }
  */
-class Solution {
-    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
-
-    }
-}
+//class Solution {
+//    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+//        var head = ListNode(0)
+//        val result = head
+//
+//        var l1 = list1
+//        var l2 = list2
+//
+//        while (l1 != null && l2 != null) {
+//            if (l1.`val` < l2.`val`) {
+//                head.next = l1
+//                l1 = l1?.next
+//            } else {
+//                head.next = l2
+//                l2 = l2?.next
+//            }
+//            head = head.next
+//        }
+//
+//        if (l1 != null) {
+//            head.next = l1
+//        } else if (l2 != null) {
+//            head.next = l2
+//        }
+//
+//        return result.next
+//
+//    }
+//}

68d58e1e0f7369642ee1befa9f8da256004eacf0
Aras <amvitkus@gmail.com>
2022-11-03 12:18:33 -0400

    Started MergeTwoSortedLists.kt.

---
+++ src/main/kotlin/exercises/mergetwosortedlists/MergeTwoSortedLists.kt
@@ -0,0 +1,41 @@
+package exercises.mergetwosortedlists
+
+/*You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+Example 1:
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]*/
+
+fun main() {
+
+}
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+
+    }
+}

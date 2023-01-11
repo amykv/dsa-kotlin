0eafcec6d3090e26791bf64e44f378d043077850
Aras <amvitkus@gmail.com>
2023-01-11 14:21:28 -0500

    feat(ctci): finished a working version of sum lists

--- src/main/kotlin/ctci/chaptertwo/SumLists.kt
+++ src/main/kotlin/ctci/chaptertwo/SumLists.kt
@@ -7,4 +7,59 @@ package ctci.chaptertwo
 
 fun main() {
 
+    val list1 = Node(9, null)
+    var current1 = list1
+    current1.next = Node(9, null)
+    current1 = current1.next!!
+    current1.next = Node(9, null)
+
+    val list2 = Node(1, null)
+    var current2 = list2
+    current2.next = Node(8, null)
+    current2 = current2.next!!
+    current2.next = Node(3, null)
+
+    val sumList = addLists(list1, list2)
+    var current = sumList
+    while (current != null) {
+        print("${current.value} ")
+        current = current.next
+    }
+    // should print "0 0 1 "
+
 }
+
+//This function takes two linked lists, each representing a number. It iterates through the two linked lists at the
+// same time, adding the corresponding digits and the carry from the previous addition. If one of the lists is
+// shorter than the other, it considers the missing digits as 0. If the final carry is greater than 0, it adds a new node with the carry value to the resulting list.
+fun addLists(l1: Node?, l2: Node?): Node? {
+    var dummyHead = Node(0, null)
+    var current = dummyHead
+    var carry = 0
+    var p1 = l1
+    var p2 = l2
+
+    while (p1 != null || p2 != null) {
+        var x = if (p1 != null) p1.value else 0
+        var y = if (p2 != null) p2.value else 0
+
+        val sum = carry + x + y
+        carry = sum / 10
+        current.next = Node(sum % 10, null)
+
+        current = current.next!!
+        if (p1 != null) p1 = p1.next
+        if (p2 != null) p2 = p2.next
+    }
+
+    if (carry > 0) {
+        current.next = Node(carry, null)
+    }
+
+    return dummyHead.next
+}
+
+//It has O(max(n,m)) time complexity, where n and m are the number of digits in the two input lists.
+// Because it iterates through both lists once, and performs a constant amount of operations for each digit.
+//
+//This solution has O(max(n,m)) space complexity because it creates a new linked list with max(n,m) elements.

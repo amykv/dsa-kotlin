d660aa74ee62e5fca0fefad875df3a8eade915f4
Aras <amvitkus@gmail.com>
2023-01-11 13:59:15 -0500

    feat(ctci): start of partition exercise

---
+++ src/main/kotlin/ctci/chaptertwo/Partition.kt
@@ -0,0 +1,10 @@
+package ctci.chaptertwo
+
+// 2.4 - page 94
+//In Kotlin, write code to partition a linked list around a value x, such that all nodes less than x come before all
+// nodes greater than or equal to x. (NOTE: The partition element x can appear anywhere in the "right partition";
+// it does not need to appear between the left and right partitions.)
+
+fun main() {
+
+}

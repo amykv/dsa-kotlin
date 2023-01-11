8f0224acaa44a7ef0306d934e9868a3881683a7a
Aras <amvitkus@gmail.com>
2023-01-11 11:54:03 -0500

    feat(ctci): start of delete middle node

---
+++ src/main/kotlin/ctci/chaptertwo/DeleteMiddleNode.kt
@@ -0,0 +1,9 @@
+package ctci.chaptertwo
+
+// 2.3 - page 94
+// In Kotlin, implement an algorithm to delete a node in the middle (i.e., any node but the first and last node, not
+// necessarily the exact middle) of a singly linked list, given only access to that node.
+
+fun main() {
+
+}

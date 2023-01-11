607251ffae8e6021c199e4daf98993f79366608e
Aras <amvitkus@gmail.com>
2023-01-11 14:14:29 -0500

    feat(ctci): start of sum lists problem

---
+++ src/main/kotlin/ctci/chaptertwo/SumLists.kt
@@ -0,0 +1,10 @@
+package ctci.chaptertwo
+
+// 2.5 - page 95
+// You have two numbers represented by a linked list, where each node contains a single digit. The digits are stored in
+// reverse order, such that the 1's digit is at the head of the list. In Kotlin, write a function that adds the two
+// numbers and returns the sum as a linked list. Not allowed to just convert the linked list to an integer.
+
+fun main() {
+
+}

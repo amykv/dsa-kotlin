6404ca4711d2a8dd834c063523602959d941b66b
arasvitkus <amvitkus@gmail.com>
2023-01-13 13:30:11 -0500

    feat(ctci): start of stack min problem

---
+++ src/main/kotlin/ctci/chapterthree/StackMin.kt
@@ -0,0 +1,9 @@
+package ctci.chapterthree
+
+// 3.2 - page 98
+// In Kotlin, how would you design a stack which, in addition to push and pop, has a function called min which returns
+// the minimum element? Push, pop, and min should all operate in O(1) time.
+
+fun main() {
+
+}

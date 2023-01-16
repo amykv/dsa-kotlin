6aa9cf3772f4103bc8ee46d3d781f06bfb19ab01
arasvitkus <amvitkus@gmail.com>
2023-01-16 14:40:19 -0500

    docs(ctci): documentation added for stack of plates problem

--- src/main/kotlin/ctci/chapterthree/StackOfPlates.kt
+++ src/main/kotlin/ctci/chapterthree/StackOfPlates.kt
@@ -1,7 +1,10 @@
 package ctci.chapterthree
 
 // 3.3 - page 99
-
+//Imagine a literal stack of plates. If the stack gets too high, it might topple. Therefore, in real life we would likely
+//start a new stack when the previous stack exceeds some threshold. In Kotlin, implement a data structure SetOfStacks
+//that mimics this. SetOfStacks should be composed of several stacks and should create a new stack once the previous one
+//exceeds capacity. SetOfStacks.push() and SetOfStacks.pop() should behave identically to a single stack.
 
 fun main() {
 

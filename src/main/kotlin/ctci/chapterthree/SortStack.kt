4ec86eec10daac1554c61fb1db793af0640bca52
arasvitkus <amvitkus@gmail.com>
2023-01-18 12:29:16 -0500

    feat(ctci): start of sort stack problem

---
+++ src/main/kotlin/ctci/chapterthree/SortStack.kt
@@ -0,0 +1,10 @@
+package ctci.chapterthree
+
+// 3.5 - page 99
+// In Kotlin, write a program to sort a stack such that the smallest items are on the top. Can use an additional
+// temporary stack, but you may not copy the elements into any other data structure(such as an array). The stack
+// supports the following operation: push, pop, peek, and isEmpty.
+
+fun main() {
+
+}

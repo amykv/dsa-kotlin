e2d3fe30f201df0c729d9c804234afe6f4ef750e
arasvitkus <amvitkus@gmail.com>
2023-01-21 13:55:50 -0500

    feat(ctci): start of minimal tree algorithm created

---
+++ src/main/kotlin/ctci/chapterfour/MinimalTree.kt
@@ -0,0 +1,9 @@
+package ctci.chapterfour
+
+// 4.2 - page 109
+// Given a sorted (increasing order) array with unique integer elements, write an algorithm in Kotlin to create a binary
+// search tree with minimal height.
+
+fun main() {
+
+}

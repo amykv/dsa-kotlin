2233b98380b23acb555e41589ffc9ced052b918d
arasvitkus <amvitkus@gmail.com>
2023-01-21 14:00:54 -0500

    feat(ctci): test algorithm completed

--- src/main/kotlin/ctci/chapterfour/MinimalTree.kt
+++ src/main/kotlin/ctci/chapterfour/MinimalTree.kt
@@ -6,8 +6,19 @@ package ctci.chapterfour
 
 fun main() {
 
+    val arr = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val root = createMinimalBST(arr, 0, arr.size - 1)
+    inOrderTraversal(root)
 }
 
+//This function uses a recursive approach to traverse the tree in-order, first traversing the left subtree, then
+// visiting the current node, and finally traversing the right subtree.
+fun inOrderTraversal(root: TreeNode?) {
+    if (root == null) return
+    inOrderTraversal(root.left)
+    println(root.value)
+    inOrderTraversal(root.right)
+}
 
 //One way to create a binary search tree with minimal height from a sorted array is to use a technique called
 // "divide and conquer". The idea is to divide the array in half, take the middle element as the root of the tree,
@@ -29,3 +40,7 @@ fun createMinimalBST(arr: IntArray, start: Int, end: Int): TreeNode? {
     node.right = createMinimalBST(arr, mid + 1, end)
     return node
 }
+
+//The time complexity of this algorithm is O(n), where n is the number of elements in the array, as it visits each
+// element of the array once. The space complexity is O(n) as well, as it recursively calls itself for each element
+// in the array.

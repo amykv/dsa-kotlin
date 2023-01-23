d978932ce0f242969232e1dbebbed719608bc94b
arasvitkus <amvitkus@gmail.com>
2023-01-23 13:40:03 -0500

    feat(ctci): start of check balanced tree problem

---
+++ src/main/kotlin/ctci/chapterfour/CheckBalanced.kt
@@ -0,0 +1,9 @@
+package ctci.chapterfour
+
+// 4.4 - page 109
+// In Kotlin, implement a function to check if a binary tree is balanced. For the purposes of this question, a balanced
+// tree is defined to be a tree such that the heights of the two subtrees of any node never differ by more than one.
+
+fun main() {
+
+}

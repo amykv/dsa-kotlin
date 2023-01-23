aa3e532d753e34930e7cc46c211b26c1a92a8c34
arasvitkus <amvitkus@gmail.com>
2023-01-23 14:19:07 -0500

    feat(ctci): start of binary to string algorithm

---
+++ src/main/kotlin/ctci/chapterfive/BinaryToString.kt
@@ -0,0 +1,5 @@
+package ctci.chapterfive
+
+// 5.2 - page 116
+// Given a real number between 0 and 1 (e.g., 0.72) that is passed in as a double, print the binary representation.
+// If the number cannot be represented accurately in binary with at most 32 characters, print "ERROR WILL ROBINSON!".

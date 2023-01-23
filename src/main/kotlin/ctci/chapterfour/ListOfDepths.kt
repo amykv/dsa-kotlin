2b04153811d5b0102180b14fe98412b2cd53d80a
arasvitkus <amvitkus@gmail.com>
2023-01-23 12:27:35 -0500

    feat(ctci): start of list of depths algorithm

---
+++ src/main/kotlin/ctci/chapterfour/ListOfDepths.kt
@@ -0,0 +1,9 @@
+package ctci.chapterfour
+
+// 4.3 - page 109
+// Given a binary tree, design an algorithm in Kotlin which creates a linked list of all the nods at each depth
+// (e.g., if you have a tree with depth D, you'll have D linked lists).
+
+fun main() {
+
+}

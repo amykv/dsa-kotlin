f5cd8c9ed3ae7665596143b25af674fa478a7e98
arasvitkus <amvitkus@gmail.com>
2023-01-28 15:01:18 -0500

    feat(ctci): finished sort merge algorithm

--- src/main/kotlin/ctci/chapterten/SortedMerge.kt
+++ src/main/kotlin/ctci/chapterten/SortedMerge.kt
@@ -5,5 +5,31 @@ package ctci.chapterten
 // in Kotlin to merge B into A in sorted order.
 
 fun main() {
-
+    val A = intArrayOf(1, 3, 5, 7, 9, 0, 0, 0, 0, 0)
+    val B = intArrayOf(2, 4, 6, 8, 10)
+    mergeSortedArrays(A, B)
+    println(A.toList())
 }
+
+
+fun mergeSortedArrays(A: IntArray, B: IntArray) {
+    // Initialize indices for the two arrays
+    var i = A.size - B.size - 1
+    var j = B.size - 1
+    var k = A.size - 1
+
+    // Compare the last elements of the two arrays and place the larger one at the end of A
+    while (j >= 0) {
+        if (i >= 0 && A[i] > B[j]) {
+            A[k] = A[i]
+            i--
+        } else {
+            A[k] = B[j]
+            j--
+        }
+        k--
+    }
+}
+//The time complexity of this algorithm is O(n), where n is the total size of the two arrays, since we are iterating
+// through both arrays once. The space complexity is O(1), since we are merging the arrays in place, without using any
+// additional data structures.

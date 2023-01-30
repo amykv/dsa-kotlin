cfe216ce9b0b438ebf7c888f2da1f190cbbcb7e0
arasvitkus <amvitkus@gmail.com>
2023-01-30 14:21:55 -0500

    feat(ctci): start of search in rotated array

--- src/main/kotlin/ctci/chapterten/SearchInRotatedArray.kt
+++ src/main/kotlin/ctci/chapterten/SearchInRotatedArray.kt
@@ -3,3 +3,11 @@ package ctci.chapterten
 // 10.3 - page 150
 // Given a sorted array of n integers that has been rotated an unknown number of times, write Kotlin code to
 // find an element in the array. You may assume that the array was originally sorted in increasing order.
+
+fun main() {
+
+}
+
+fun findArrayElement() {
+
+}

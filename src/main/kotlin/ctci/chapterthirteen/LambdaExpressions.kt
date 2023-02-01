18fe3e8d841646d3f1c5af0c227ad4c5af24d58e
arasvitkus <amvitkus@gmail.com>
2023-02-01 14:23:25 -0500

    docs(ctci): documentation related to chapter thirteen

---
+++ src/main/kotlin/ctci/chapterthirteen/LambdaExpressions.kt
@@ -0,0 +1,6 @@
+package ctci.chapterthirteen
+
+// 11.7 - page 168
+// There is a class called Country that has methods getContinent() and getPopulation(). Write a Kotlin function int
+// getPopulation(List<Country> countries, String continent) that computes the total population of a given continent,
+// given a list of all countries and the name of a continent.

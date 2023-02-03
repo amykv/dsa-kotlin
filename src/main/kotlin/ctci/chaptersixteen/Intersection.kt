2082e01c65c4138a5754e93ce15dac061371177e
arasvitkus <amvitkus@gmail.com>
2023-02-03 14:30:51 -0500

    feat(ctci): intersection algorithm started

---
+++ src/main/kotlin/ctci/chaptersixteen/Intersection.kt
@@ -0,0 +1,9 @@
+package ctci.chaptersixteen
+
+// 16.3 - page 181
+// Given two straight line segments (represented as a start point and an end point), compute the point of
+// intersection, if any.
+
+fun main() {
+
+}

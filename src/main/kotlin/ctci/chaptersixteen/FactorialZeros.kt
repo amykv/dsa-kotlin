59dca0fda9d465ebb5b86f6adcdfcc517261f83d
arasvitkus <amvitkus@gmail.com>
2023-02-05 15:22:32 -0500

    feat(ctci): implemented trailing zeros algorithm

--- src/main/kotlin/ctci/chaptersixteen/FactorialZeros.kt
+++ src/main/kotlin/ctci/chaptersixteen/FactorialZeros.kt
@@ -4,5 +4,21 @@ package ctci.chaptersixteen
 // Write an algorithm in Kotlin which computes the number of trailing zeros in n factorial
 
 fun main(){
-
+    val n = 20
+    println("Number of trailing zeros in $n!: ${countTrailingZeros(n)}")
 }
+
+fun countTrailingZeros(n: Int): Int {
+    var count = 0
+    for (i in 5..n step 5) {
+        var num = i
+        while (num % 5 == 0) {
+            count++
+            num /= 5
+        }
+    }
+    return count
+}
+
+//The time complexity of this algorithm is O(n) and the space complexity is O(1), since we only use a few variables
+// and don't allocate any extra memory.

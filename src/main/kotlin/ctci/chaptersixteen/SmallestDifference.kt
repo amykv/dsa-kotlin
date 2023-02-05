9f488a5337281cdea43377bb51bba0c73dcf8b5c
arasvitkus <amvitkus@gmail.com>
2023-02-05 16:47:32 -0500

    feat(ctci): start of smallest difference problem

---
+++ src/main/kotlin/ctci/chaptersixteen/SmallestDifference.kt
@@ -0,0 +1,5 @@
+package ctci.chaptersixteen
+
+// 16.6 - page 181
+// Given two arrays of integers, compute the pair of values (one value in each array) with smallest (non-negative)
+// difference. Return the difference.

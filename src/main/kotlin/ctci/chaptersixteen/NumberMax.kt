119eb0ac4f55c49a8951d761109b2df979ff60dd
arasvitkus <amvitkus@gmail.com>
2023-02-06 11:34:30 -0500

    feat(ctci): start of number max algorithm

---
+++ src/main/kotlin/ctci/chaptersixteen/NumberMax.kt
@@ -0,0 +1,6 @@
+package ctci.chaptersixteen
+
+// 16.7 - page 181
+// In Kotlin, write a method that finds the maximum of two numbers. You should not use if-else or any other
+// comparison operator.
+

b94d754d121f3cff175f13cc918aba45e4ad6a7b
arasvitkus <amvitkus@gmail.com>
2023-02-06 11:35:47 -0500

    feat(ctci): finished number max algorithm

--- src/main/kotlin/ctci/chaptersixteen/NumberMax.kt
+++ src/main/kotlin/ctci/chaptersixteen/NumberMax.kt
@@ -4,3 +4,21 @@ package ctci.chaptersixteen
 // In Kotlin, write a method that finds the maximum of two numbers. You should not use if-else or any other
 // comparison operator.
 
+fun main() {
+    val a = 5
+    val b = 10
+    println("Max of $a and $b is ${max(a, b)}")
+}
+
+//This code uses the bitwise right shift operator shr and the bitwise and operator and to perform the comparison.
+// The expression c shr 31 will either return 0 or 1, depending on the sign of c. If c is negative, meaning a is less
+// than b, then k will be 1. In that case, the expression a - k * c will be equal to b. If c is non-negative,
+// meaning a is greater than or equal to b, then k will be 0, and the expression a - k * c will be equal to a.
+fun max(a: Int, b: Int): Int {
+    val c = a - b
+    val k = c shr 31 and 1
+    return a - k * c
+}
+
+//The time complexity of this method is O(1) since it only performs simple arithmetic operations and bitwise
+// operations. The space complexity is also O(1), as it only uses a few variables.

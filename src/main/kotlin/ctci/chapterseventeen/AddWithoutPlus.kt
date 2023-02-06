065b08f6955637f7c2d5f34d72b8e662f70bdc38
arasvitkus <amvitkus@gmail.com>
2023-02-06 11:43:35 -0500

    feat(ctci): finished add without plus

--- src/main/kotlin/ctci/chapterseventeen/AddWithoutPlus.kt
+++ src/main/kotlin/ctci/chapterseventeen/AddWithoutPlus.kt
@@ -11,15 +11,22 @@ fun main() {
 }
 
 fun add(a: Int, b: Int): Int {
-    var x = a
-    var y = b
-    while (y != 0) {
-        val carry = x and y
-        x = x xor y
-        y = carry shl 1
+    // We use bitwise operations to add two numbers without using the + operator.
+    // We repeatedly add the carry bit to the sum of the two numbers until there's no carry bit left.
+    var sum = a
+    var carry = b
+    while (carry != 0) {
+        // Calculate the sum of the two numbers without considering the carry bit.
+        val temp = sum xor carry
+        // Calculate the carry bit, which is the result of the binary AND operation between the sum and carry.
+        carry = sum and carry shl 1
+        // Update the sum to include the carry bit.
+        sum = temp
     }
-    return x
+    // Return the final sum.
+    return sum
 }
+
 //The space complexity of this function is O(1), as it uses only a few variables. The time complexity is O(n), where
 // n is the number of bits required to represent the larger of the two input numbers. This is because the while loop
 // will iterate at most n times, and each iteration performs a small constant amount of work.

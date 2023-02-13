a07dc245028096ce225844f529f5e2be103ead8a
arasvitkus <amvitkus@gmail.com>
2023-02-13 12:00:15 -0500

    feat(ctci): start of sum swap algorithm

---
+++ src/main/kotlin/ctci/chaptersixteen/SumSwap.kt
@@ -0,0 +1,4 @@
+package ctci.chaptersixteen
+
+// 16.21 - page 184
+// Given two arrays of integers, find a pair of values (one value from each array) that you can swap to give the two arrays the same sum.

2159ed6b32a9a106a61cb20b29ad61d6c19b6af9
arasvitkus <amvitkus@gmail.com>
2023-02-21 12:58:15 -0500

    feat(ctci): start of contiguous sequence

---
+++ src/main/kotlin/ctci/chaptersixteen/ContiguousSequence.kt
@@ -0,0 +1,5 @@
+package ctci.chaptersixteen
+
+// 16.17 - page 183
+// You are given an array of integers (both positive and negative). Find the contiguous sequence with the largest
+// sum. Return the sum.

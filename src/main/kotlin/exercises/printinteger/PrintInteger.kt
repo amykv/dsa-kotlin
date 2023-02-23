39bad06257e1415b5fb143b03405c8cf554d981e
arasvitkus <amvitkus@gmail.com>
2023-02-23 14:29:12 -0500

    feat(exercises): implemented print integer example

---
+++ src/main/kotlin/exercises/printinteger/PrintInteger.kt
@@ -0,0 +1,19 @@
+package exercises.printinteger
+
+import java.util.*
+
+// How to print an integer entered by user in Kotlin using Scanner
+
+fun main(args: Array<String>) {
+
+    // Creates a reader instance which takes
+    // input from standard input - keyboard
+    val reader = Scanner(System.`in`)
+    print("Enter a number: ")
+
+    // nextInt() reads the next integer from the keyboard
+    val integer:Int = reader.nextInt()
+
+    // println() prints the following line to the output screen
+    println("You entered: $integer")
+}

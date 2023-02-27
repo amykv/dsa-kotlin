0ac52e2f429467564980494bff1746a38f84dd99
arasvitkus <amvitkus@gmail.com>
2023-02-27 12:52:44 -0500

    feat(exercises): implemented check leap year

---
+++ src/main/kotlin/exercises/checkleapyear/CheckLeapYear.kt
@@ -0,0 +1,19 @@
+package exercises.checkleapyear
+
+// Check if Leap Year
+fun main(args: Array<String>) {
+
+    val year = 1900
+    var leap = false
+
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            // year is divisible by 400, hence the year is a leap year
+            leap = year % 400 == 0
+        } else
+            leap = true
+    } else
+        leap = false
+
+    println(if (leap) "$year is a leap year." else "$year is not a leap year.")
+}

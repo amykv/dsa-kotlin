0495fb1c4c02e17c1e8a24f22e56bc4877a7cd65
arasvitkus <amvitkus@gmail.com>
2023-02-28 00:23:47 -0500

    feat(exercises): implemented find largest element

---
+++ src/main/kotlin/exercises/largestelement/LargestElement.kt
@@ -0,0 +1,15 @@
+package exercises.largestelement
+
+// Program to find largest element in array
+
+fun main(args: Array<String>) {
+    val numArray = doubleArrayOf(23.4, -34.5, 50.0, 33.5, 55.5, 43.7, 5.7, -66.5)
+    var largest = numArray[0]
+
+    for (num in numArray) {
+        if (largest < num)
+            largest = num
+    }
+
+    println("Largest element = %.2f".format(largest))
+}

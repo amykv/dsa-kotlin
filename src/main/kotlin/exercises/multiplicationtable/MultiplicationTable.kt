7705db0b551161e09ba604f6e4bff6004f3f6c72
arasvitkus <amvitkus@gmail.com>
2023-02-28 00:21:45 -0500

    feat(exercises): implemented multiplication table

---
+++ src/main/kotlin/exercises/multiplicationtable/MultiplicationTable.kt
@@ -0,0 +1,11 @@
+package exercises.multiplicationtable
+
+// Program to generate multiplication table
+fun main(args: Array<String>) {
+    val num = 5
+
+    for (i in 1..10) {
+        val product = num * i
+        println("$num * $i = $product")
+    }
+}

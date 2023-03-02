1d9e6499996f79564d2b5f4821b77601eb813645
amvitkus <amvitkus@gmail.com>
2023-03-01 22:00:34 -0500

    refactor(exercises): exception handling added

--- src/main/kotlin/exercises/checkevenodd/EvenOrOdd.kt
+++ src/main/kotlin/exercises/checkevenodd/EvenOrOdd.kt
@@ -1,18 +1,21 @@
 package exercises.checkevenodd
 
 import java.util.*
-
 //Check if a number is even or odd
 
 fun main() {
 
     val reader = Scanner(System.`in`)
+    try {
 
     print("Enter a number: ")
     val num = reader.nextInt()
 
-    if (num % 2 == 0)
-        println("$num is even.")
-    else
-        println("$num is odd.")
+        if (num % 2 == 0)
+            println("$num is even.")
+        else
+            println("$num is odd.")
+    } catch (e: InputMismatchException) {
+        println("Enter a valid number. Program will close now.")
+    }
 }

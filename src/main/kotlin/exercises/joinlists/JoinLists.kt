3b4bb1d5558f3a76e51ac38a01c1d356291af219
arasvitkus <amvitkus@gmail.com>
2023-03-06 12:45:52 -0500

    feat(exercises): finished join lists example

--- src/main/kotlin/exercises/joinlists/JoinLists.kt
+++ src/main/kotlin/exercises/joinlists/JoinLists.kt
@@ -1,2 +1,24 @@
 package exercises.joinlists
 
+import java.util.ArrayList
+
+//Kotlin program to join two lists together
+
+fun main() {
+
+    val list1 = ArrayList<String>()
+    list1.add("a, t, f")
+
+    val list2 = ArrayList<String>()
+    list2.add("b, x, z")
+
+    val joined = ArrayList<String>()
+
+    joined.addAll(list1)
+    joined.addAll(list2)
+
+    println("list1: $list1")
+    println("list2: $list2")
+    println("Joined: $joined")
+
+}

2cea089c5982870569ce4149efee069ba2e8dccc
arasvitkus <amvitkus@gmail.com>
2023-03-08 13:33:14 -0500

    refactor(exercises): line indentation change

--- src/main/kotlin/exercises/isomorphicstrings/IsomorphicStrings.kt
+++ src/main/kotlin/exercises/isomorphicstrings/IsomorphicStrings.kt
@@ -4,7 +4,8 @@ package exercises.isomorphicstrings
 //
 //Two strings s and t are isomorphic if the characters in s can be replaced to get t.
 //
-//All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+//All occurrences of a character must be replaced with another character while preserving the order of characters.
+// No two characters may map to the same character, but a character may map to itself.
 //
 //
 //

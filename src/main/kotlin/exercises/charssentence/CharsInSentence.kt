988415d869dc9ce40bd5d3ffc6e15887462f5912
arasvitkus <amvitkus@gmail.com>
2023-03-15 19:33:03 -0400

    refactor(exercises): line indentation change

--- src/main/kotlin/exercises/charssentence/CharsInSentence.kt
+++ src/main/kotlin/exercises/charssentence/CharsInSentence.kt
@@ -1,7 +1,6 @@
 package exercises.charssentence
 
 // Program that counts how many characters in a sentence.
-
 fun main() {
     print("Enter a sentence: ")
     val sentence = readlnOrNull() ?: ""

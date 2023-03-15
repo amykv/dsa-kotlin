7c2e791e3170eb11d8f9fbbfc5f8177dad324ce8
arasvitkus <amvitkus@gmail.com>
2023-03-15 19:33:51 -0400

    refactor(exercises): line indentation change

--- src/main/kotlin/exercises/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/exercises/fizzbuzz/FizzBuzz.kt
@@ -7,8 +7,6 @@ answer[i] == "Fizz" if i is divisible by 3.
 answer[i] == "Buzz" if i is divisible by 5.
 answer[i] == i (as a string) if none of the above conditions are true.
 
-
-
 Example 1:
 
 Input: n = 3

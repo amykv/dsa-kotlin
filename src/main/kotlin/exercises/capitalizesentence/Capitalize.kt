f3ac075fb665b8f061ff6f2c6d72cd5439be7db1
arasvitkus <amvitkus@gmail.com>
2023-03-17 17:56:15 -0400

    feat(exercises): finished capitalize sentence

--- src/main/kotlin/exercises/capitalizesentence/Capitalize.kt
+++ src/main/kotlin/exercises/capitalizesentence/Capitalize.kt
@@ -1,2 +1,20 @@
 package exercises.capitalizesentence
+//Program to capitalize the first letter in each word
 
+fun main() {
+    val words = "Here are some words"
+    println(capitalizeSentence(words))
+
+}
+
+// Iterative solution
+
+    private fun capitalizeSentence(str: String): String {
+        val words = mutableListOf<String>()
+
+        str.split(" ").forEach {
+            words.add(it[0].uppercase() + it.substring(1))
+        }
+
+        return words.joinToString(" ")
+    }

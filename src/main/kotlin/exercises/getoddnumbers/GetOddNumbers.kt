de7e7a32e01b158ec4f2961ff71ea56fc25677e3
arasvitkus <amvitkus@gmail.com>
2023-03-18 11:52:51 -0400

    feat(exercises): start of get odd numbers

--- src/main/kotlin/exercises/getoddnumbers/GetOddNumbers.kt
+++ src/main/kotlin/exercises/getoddnumbers/GetOddNumbers.kt
@@ -1,3 +1,11 @@
 package exercises.getoddnumbers
 
 //Kotlin program to filter and print odd numbers from a list
+
+fun main(){
+    println(filterOdd(listOf(1, 2, 3, 4, 5, 6, 7, 8, 11, 12, 16, 17, 18, 20)))
+}
+
+private fun filterOdd(list: List<Int>): List<Int> {
+    return list.filter { it % 2 == 1 }
+}

47d0a02d045ba3f7395d288d62d62af1650bfd53
arasvitkus <amvitkus@gmail.com>
2023-03-19 15:51:47 -0400

    feat(exercises): finished quick sort low/high

--- src/main/kotlin/exercises/lowesthighestquicksort/LowHigh.kt
+++ src/main/kotlin/exercises/lowesthighestquicksort/LowHigh.kt
@@ -12,5 +12,44 @@ package exercises.lowesthighestquicksort
 // on the right side of the pivot)
 
 fun main() {
+    val unsortedList = mutableListOf(5, 2, 9, 1, 5, 6, 3)
+    val sortedList = quickSort(unsortedList)
+    println(sortedList)
+}
 
+// Time complexity: O(n^2)
+// Space complexity: O(n)
+private fun quickSort(list: MutableList<Int>): List<Number> {
+    if (list.isEmpty()) {
+        return list
+    }
+
+    val pivot = list.first() // first element as pivot (it can be any element)
+    var pivotIndex = 0 // first index that can swap (number of element that are less than pivot)
+
+    (0..list.lastIndex).forEach {
+        if (pivot > list[it]) {
+            list.swap(it, pivotIndex + 1)
+            pivotIndex++
+        }
+    }
+
+    // Move element to the correct index
+    // All elements smaller than element will be on the left side of the array (smaller indexes)
+    // All elements larger than element will be on the left side of the array (lager indexes)
+    list.swap(0, pivotIndex)
+
+    // Create left sub-list
+    val left = list.subList(0, pivotIndex)
+
+    // Create right sub-list
+    val right = list.subList(pivotIndex + 1, list.size)
+
+    return quickSort(left) + listOf(pivot) + quickSort(right)
+}
+
+private fun <T> MutableList<T>.swap(index1: Int, index2: Int) {
+    val tmp = this[index1]
+    this[index1] = this[index2]
+    this[index2] = tmp
 }

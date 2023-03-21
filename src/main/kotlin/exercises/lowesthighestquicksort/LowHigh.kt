5a0a2c10da9974566be2604a2ce18252238f0631
arasvitkus <amvitkus@gmail.com>
2023-03-20 21:30:05 -0400

    feat(exercises): finished quick sort low/high

--- src/main/kotlin/exercises/lowesthighestquicksort/LowHigh.kt
+++ src/main/kotlin/exercises/lowesthighestquicksort/LowHigh.kt
@@ -2,17 +2,8 @@ package exercises.lowesthighestquicksort
 
 //Sort list of numbers from lowest number to greatest number using quick sort.
 //
-//Algorithm:
-//
-//Pick first element as pivot (pivot can be also last, random element but we explicitly always pick first element)
-//Partition elements - start traversing from the leftmost element and if a smaller element is found, swap current
-// element with list[i]. Keep track of index of smaller to elements at index (pivotIndex).
-//Swap pivot with element at pivotIndex
-//Recursively repeat the process for left partition (element on the left side of pivot) and right partition (element
-// on the right side of the pivot)
-
 fun main() {
-    val unsortedList = mutableListOf(5, 2, 9, 1, 5, 6, 3)
+    val unsortedList = mutableListOf(5, 2, 9, 1, 5, 6, 3, 11, 55, 23, 21, 99)
     val sortedList = quickSort(unsortedList)
     println(sortedList)
 }

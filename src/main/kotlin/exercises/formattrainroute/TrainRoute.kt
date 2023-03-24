bed45cae9a72b22021b9ea75f99e231aa42cbeb2
arasvitkus <amvitkus@gmail.com>
2023-03-24 17:18:52 -0400

    feat(exercises): implemented train route solution

--- src/main/kotlin/exercises/formattrainroute/TrainRoute.kt
+++ src/main/kotlin/exercises/formattrainroute/TrainRoute.kt
@@ -21,3 +21,19 @@ package exercises.formattrainroute
 fun main() {
 
 }
+
+private fun formatTrainRoute(stations: List<String>): String {
+    val prefix = "Train is calling at "
+
+    if (stations.size == 1) {
+        return prefix + stations[0]
+    }
+
+    val lastStation = stations.takeLast(1)
+        .joinToString { it }
+
+    val followingStations = stations.dropLast(1)
+        .joinToString { it }
+
+    return "$prefix$followingStations and $lastStation"
+}

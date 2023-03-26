69c1b44f9fbcb508843f73ad36c8b841183301a3
arasvitkus <amvitkus@gmail.com>
2023-03-26 11:18:54 -0400

    feat(exercises): start of product challenge

---
+++ src/main/kotlin/exercises/product/Product.kt
@@ -0,0 +1,14 @@
+package exercises.product
+
+//Instructions
+//
+//Given a list of integers implement a function which returns the product of all numbers in the list.
+//
+//Challenge | Solution
+//Examples
+//
+//product(listOf(1, 2)) // 2
+//
+//product(listOf(1, 2, 4)) // 8
+//
+//product(listOf(2, 4, 10)) // 80

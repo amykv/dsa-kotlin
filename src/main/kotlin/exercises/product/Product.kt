512c7bc34d62003389cde6ca14e16489200a2187
arasvitkus <amvitkus@gmail.com>
2023-03-26 11:21:54 -0400

    feat(exercises): implemented solution for product challenge

--- src/main/kotlin/exercises/product/Product.kt
+++ src/main/kotlin/exercises/product/Product.kt
@@ -12,3 +12,27 @@ package exercises.product
 //product(listOf(1, 2, 4)) // 8
 //
 //product(listOf(2, 4, 10)) // 80
+
+fun main() {
+    println(product(listOf(1, 2))) // 2
+    println(product(listOf(1, 2, 4))) // 8
+    println(product(listOf(2, 4, 10))) // 80
+}
+fun product(numbers: List<Int>): Int {
+    if (numbers.isEmpty()) {
+        return 0
+    }
+
+    var result = 1
+    for (number in numbers) {
+        result *= number
+    }
+    return result
+}
+
+//The time complexity of this code is O(n), where n is the length of the input list. This is because the function
+//performs a single loop over the elements of the input list, and performs a constant amount of work for each element.
+//Therefore, the amount of work performed by the function is proportional to the length of the input list.
+//
+//The space complexity of this code is O(1), because the function only uses a constant amount of additional memory
+//regardless of the size of the input list.

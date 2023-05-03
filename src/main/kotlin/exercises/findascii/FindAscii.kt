e0ffa1dfe207357e20c8391f08f517941f8a696f
amvitkus <amvitkus@gmail.com>
2023-05-03 17:16:01 -0400

    chore(exercises): remove deprecated code

--- src/main/kotlin/exercises/findascii/FindAscii.kt
+++ src/main/kotlin/exercises/findascii/FindAscii.kt
@@ -3,8 +3,9 @@ package exercises.findascii
 //Find ASCII value of a character
 fun main() {
 
+    //Set values
     val c = 'a'
-    val ascii = c.toInt()
+    val ascii = c.code //.toInt() is deprecated
 
     println("The ASCII value of $c is: $ascii")
 }

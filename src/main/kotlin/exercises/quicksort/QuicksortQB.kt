3e75a776ada6a91a33c8f9f87e0ffc4e6b3efc34
amvitkus <amvitkus@gmail.com>
2023-05-21 13:33:20 -0400

    feat(exercises): start of quicksort

--- src/main/kotlin/exercises/quicksort/QuicksortQB.kt
+++ src/main/kotlin/exercises/quicksort/QuicksortQB.kt
@@ -5,6 +5,7 @@ package exercises.quicksort
 data class Quarterback(val name: String, val passingYards: Int)
 
 fun main() {
+    // Define a list of Quarterback objects with their names and passing yards
     val quarterbacks = listOf(
         Quarterback("Tom Brady", 5814),
         Quarterback("Patrick Mahomes", 4740),
@@ -13,44 +14,65 @@ fun main() {
         Quarterback("Matthew Stafford", 4208)
     )
 
+    // Convert the list to a mutable list
     val mutableQuarterbacks = quarterbacks.toMutableList()
 
+    // Print the quarterbacks list before sorting
     println("Before sorting:")
     mutableQuarterbacks.forEach { println("${it.name}: ${it.passingYards} yards") }
 
+    // Sort the mutableQuarterbacks list using the quickSort function
     quickSort(mutableQuarterbacks, 0, mutableQuarterbacks.size - 1) { qb1, qb2 ->
         qb1.passingYards - qb2.passingYards
     }
 
+    // Print the quarterbacks list after sorting
     println("\nAfter sorting:")
     mutableQuarterbacks.forEach { println("${it.name}: ${it.passingYards} yards") }
 }
 
+// QuickSort implementation
 fun <T> quickSort(list: MutableList<T>, low: Int, high: Int, comparator: (T, T) -> Int) {
+    // Base case: If there are at least two elements
     if (low < high) {
+        // Partition the list and get the pivot index
         val pivotIndex = partition(list, low, high, comparator)
+
+        // Recursively sort the left partition
         quickSort(list, low, pivotIndex - 1, comparator)
+
+        // Recursively sort the right partition
         quickSort(list, pivotIndex + 1, high, comparator)
     }
 }
 
+// Partition function for QuickSort
 fun <T> partition(list: MutableList<T>, low: Int, high: Int, comparator: (T, T) -> Int): Int {
+    // Select the last element as the pivot
     val pivot = list[high]
     var i = low - 1
 
+    // Iterate through the subarray
     for (j in low until high) {
+        // If the current element is smaller than the pivot
         if (comparator(list[j], pivot) < 0) {
             i++
+
+            // Swap list[i] and list[j]
             list.swap(i, j)
         }
     }
 
+    // Swap list[i+1] and list[high] (placing the pivot in its correct position)
     list.swap(i + 1, high)
     return i + 1
 }
 
+// Extension function to swap elements in a MutableList
 fun <T> MutableList<T>.swap(index1: Int, index2: Int) {
     val temp = this[index1]
     this[index1] = this[index2]
     this[index2] = temp
 }
+
+//Space and time complexity is O(log n)

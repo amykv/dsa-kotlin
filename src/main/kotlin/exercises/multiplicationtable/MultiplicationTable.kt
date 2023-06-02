7eea3f3aadbd05c583ab8267658e37cb965ffa8b
amvitkus <amvitkus@gmail.com>
2023-06-02 18:11:11 -0400

    chore(exercises): line comment added

--- src/main/kotlin/exercises/multiplicationtable/MultiplicationTable.kt
+++ src/main/kotlin/exercises/multiplicationtable/MultiplicationTable.kt
@@ -4,6 +4,7 @@ package exercises.multiplicationtable
 fun main() {
     val num = 5
 
+    // For loop for i in 1 through 10
     for (i in 1..10) {
         val product = num * i
         println("$num * $i = $product")

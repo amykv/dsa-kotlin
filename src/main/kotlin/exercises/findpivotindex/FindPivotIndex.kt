53f00de37d12401bd79e6198965d50a96111eb7c
amvitkus <amvitkus@gmail.com>
2023-06-04 20:57:52 -0400

    chore(exercises): line adjustment

--- src/main/kotlin/exercises/findpivotindex/FindPivotIndex.kt
+++ src/main/kotlin/exercises/findpivotindex/FindPivotIndex.kt
@@ -11,7 +11,6 @@ also applies to the right edge of the array.
 
 Return the leftmost pivot index. If no such index exists, return -1.
 
-
 Example 1:
 
 Input: nums = [1,7,3,6,5,6]
@@ -20,7 +19,6 @@ Explanation:
 The pivot index is 3.
 Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
 Right sum = nums[4] + nums[5] = 5 + 6 = 11
-
 
 Example 2:
 
